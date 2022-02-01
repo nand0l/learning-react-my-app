1a54cd26f1a27d5627d938c13caadd3c1f314d79
John Doe <johndoe@example.com>
2022-02-01 13:16:59 +0100

    Initial Build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Heading level 1</h1>
+        <h2>Heading level 2</h2>
+        <h3>Heading level 3</h3>
+        <h4>Heading level 4</h4>
+        <h5>Heading level 5</h5>
+        <h6>Heading level 6</h6>
       </header>
+      <p>I really like using React.</p>
+      <p>I think I'll use it to format all of my documents from now on.</p>
+      <p>This is the first line.
+        And this is the second line.</p>
     </div>
   );
 }
